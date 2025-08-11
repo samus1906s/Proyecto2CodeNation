55e5803eeedb4fa3e54617efd1c230a7922395cb
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-08-11 16:49:32 -0600

    Clase Reserva
    Se crea la clase Reserva con sus respetivos metodos get, set y constructor

--- src/Entidades/Reserva.java
+++ src/Entidades/Reserva.java
@@ -4,10 +4,68 @@
  */
 package Entidades;
 
+import Validaciones.ValidarReservas;
+import java.time.LocalDate;
+import Entidades.Clientes;
+import Entidades.Vehiculos;
+import java.util.Map;
+
 /**
  *
- * @author je110
+ * @author Eduard Salas Murillo
  */
 public class Reserva {
-    
+    private int idReserva;
+    private Clientes cedulaCliente;
+    private TipoVehiculo tipoVehiculo;
+    private Vehiculos placaVehiculo;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private EstadoContrato estado;
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public Clientes getCedulaCliente() {
+        return cedulaCliente;
+    }
+
+    public TipoVehiculo getTipoVehiculo() {
+        return tipoVehiculo;
+    }
+
+    public Vehiculos getPlacaVehiculo() {
+        return placaVehiculo;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public EstadoContrato getEstado() {
+        return estado;
+    }
+
+    public Reserva(int idReserva, Clientes cedulaCliente, TipoVehiculo tipoVehiculo, Vehiculos placaVehiculo, LocalDate fechaInicio,LocalDate fechaFin, EstadoContrato estado, Map<String, Clientes> clientes, Map<Integer, Reserva> reservas, Map<String, Vehiculos> vehiculos) {
+        this.idReserva = idReserva;
+        if(ValidarReservas.ClienteRegistrado(cedulaCliente, clientes)) 
+        this.cedulaCliente = cedulaCliente ;
+        if(ValidarReservas.VehiculoRegistrado(placaVehiculo, vehiculos))
+        this.tipoVehiculo = tipoVehiculo;
+        this.placaVehiculo = placaVehiculo;
+        if (ValidarReservas.FechaInicioValida(fechaInicio))
+        this.fechaInicio = fechaInicio;
+        if(ValidarReservas.FechaFinPosterior(fechaInicio, fechaFin))
+        this.fechaFin = fechaFin;
+        if(ValidarReservas.DuracionValida(fechaInicio, fechaFin))
+        this.fechaFin =fechaFin;
+        if(ValidarReservas.VehiculoDisponible(placaVehiculo, fechaInicio, fechaFin,reservas ))
+        this.estado = estado;       
+    }
 }
+ 
