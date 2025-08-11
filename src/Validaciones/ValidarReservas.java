edb7a4dcd8f07fb8b27a4e2c62b054fc9da63d18
eduardsalas <edusalasmurqest.utn.ac.cr>
2025-08-11 16:47:15 -0600

    Clases Validaciones
    Se crean las validaciones para las clases Personas y Reservas

--- src/Validaciones/ValidarReservas.java
+++ src/Validaciones/ValidarReservas.java
@@ -4,10 +4,56 @@
  */
 package Validaciones;
 
+import Entidades.Reserva;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import Entidades.Clientes;
+import Entidades.Vehiculos;
+
+
+
 /**
  *
- * @author je110
+ * @author Eduard Salas Murillo
  */
-public class ValidarReservas {
+public abstract class ValidarReservas {
     
+    public static boolean ClienteRegistrado(Clientes cedula, Map<String,Clientes> clientes) {
+        return clientes.containsKey(cedula);
+    }
+
+    public static boolean VehiculoRegistrado(Vehiculos placa, Map<String,Vehiculos> vehiculos) {
+        return vehiculos.containsKey(placa);
+    }
+
+    public static boolean FechaInicioValida(LocalDate fechaInicio) {
+        return !fechaInicio.isBefore(LocalDate.now());
+    }
+
+    public static boolean FechaFinPosterior(LocalDate fechaInicio, LocalDate fechaFin) {
+        return fechaFin.isAfter(fechaInicio);
+    }
+
+    public static boolean DuracionValida(LocalDate fechaInicio, LocalDate fechaFin) {
+        long dias = ChronoUnit.DAYS.between(fechaInicio, fechaFin);
+        return dias <= 30;
+    }
+
+    public static boolean VehiculoDisponible(
+            Vehiculos placa,
+            LocalDate fechaInicio,
+            LocalDate fechaFin,
+            Map<Integer,Reserva> reservas
+    ) {
+        for (Reserva r : reservas.values()) {
+            if (r.getPlacaVehiculo().equals(placa)) {
+                boolean solapado = !(fechaFin.isBefore(r.getFechaInicio()) || fechaInicio.isAfter(r.getFechaFin()));
+                if (solapado) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
