3325f28f0716720823011cea4ad697855108813c
samus <samuelcedas19@gmail.com>
2025-08-12 00:26:11 -0600

    Empleado, Enum de empledos y Validaciones de empleado

--- src/Entidades/Empleados.java
+++ src/Entidades/Empleados.java
@@ -4,10 +4,50 @@
  */
 package Entidades;
 
+import java.time.LocalDate;
+import Validaciones.ValidarEmpleados;
+
 /**
  *
  * @author je110
  */
-public class Empleados {
+public class Empleados extends Persona{
+    private PuestoEmpleado puesto;
+    private double salario;
+
+    public PuestoEmpleado getPuesto() {
+        return puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setPuesto(PuestoEmpleado puesto) {
+        this.puesto = puesto;
+    }
+
+    public void setSalario(double salario) {
+        if(ValidarEmpleados.validarSalario(salario))
+           this.salario = salario;
+    }
+    
+
+    public Empleados( String cedula, String nombre, LocalDate fechaNacimiento) {
+        super(cedula, nombre, fechaNacimiento);
+        if(ValidarEmpleados.validarPuesto(puesto.getFuncion())){
+            this.puesto = puesto;
+        }else{
+            this.puesto = PuestoEmpleado.SECRETARIO;
+        }
+        if(ValidarEmpleados.validarSalario(salario)){
+          this.salario = salario;
+        }else
+          this.salario = 0;
+    }
+    
+    
+    
+    
     
 }
--- src/Validaciones/ValidarEmpleados.java
+++ src/Validaciones/ValidarEmpleados.java
@@ -3,11 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Validaciones;
+import Entidades.PuestoEmpleado;
 
 /**
  *
  * @author je110
  */
 public class ValidarEmpleados {
+    private static final double salarioMinimo = 300000;
+    private static final double salarioMaximo = 500000;
     
+    public static boolean validarPuesto(String cargo){
+        return PuestoEmpleado.buscarPorNombre(cargo) != null;
+    }
+    
+    public static boolean validarSalario(double saldo){
+        return saldo >= salarioMinimo && saldo <= salarioMaximo;
+    }
+    
+    public static double obtenerSalarioMinimo() {
+        return salarioMinimo;
+    }
+    
+    public static double obtenerSalarioMaximo() {
+        return salarioMaximo;
+    }
+
 }
